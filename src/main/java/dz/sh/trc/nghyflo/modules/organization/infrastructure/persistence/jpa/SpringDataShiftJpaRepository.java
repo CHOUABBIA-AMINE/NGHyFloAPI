3a9b36fb0a893037ac6f8f660e5fd672dd9ef7de
Aminosoft <147345045+CHOUABBIA-AMINE@users.noreply.github.com>
2026-05-22 23:49:44 +0100

    persistence(organization): add shift Spring Data repository

---
+++ src/main/java/dz/sh/trc/nghyflo/modules/organization/infrastructure/persistence/jpa/SpringDataShiftJpaRepository.java
@@ -0,0 +1,6 @@
+package dz.sh.trc.nghyflo.modules.organization.infrastructure.persistence.jpa;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SpringDataShiftJpaRepository extends JpaRepository<ShiftJpaEntity, String> {
+}
