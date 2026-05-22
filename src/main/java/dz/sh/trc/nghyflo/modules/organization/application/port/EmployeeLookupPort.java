6921da34a50bb60b6528731764e131b6753a0a72
Aminosoft <147345045+CHOUABBIA-AMINE@users.noreply.github.com>
2026-05-22 16:14:56 +0100

    feat(organization-app): add employee lookup port

---
+++ src/main/java/dz/sh/trc/nghyflo/modules/organization/application/port/EmployeeLookupPort.java
@@ -0,0 +1,28 @@
+/**
+ *
+ * @Project     : NGHyFloAPI
+ * @Product     : NGHyFlo - New Generation Hydrocarbon Flow Intelligence Platform
+ * @Author      : NGHyFlo Engineering Team
+ * @Owner       : Sonatrach / TRC Digitalization Initiative
+ *
+ * @Name        : EmployeeLookupPort
+ * @CreatedOn   : 2026-05-22
+ * @UpdatedOn   : 2026-05-22
+ *
+ * @Type        : Interface
+ * @Layer       : Application
+ * @Package     : dz.sh.trc.nghyflo.modules.organization.application.port
+ *
+ * @Description : Application port for resolving organization employees.
+ *
+ */
+package dz.sh.trc.nghyflo.modules.organization.application.port;
+
+import dz.sh.trc.nghyflo.modules.organization.domain.model.Employee;
+import dz.sh.trc.nghyflo.shared.domain.value.EmployeeId;
+import java.util.Optional;
+
+public interface EmployeeLookupPort {
+
+    Optional<Employee> findById(EmployeeId employeeId);
+}
