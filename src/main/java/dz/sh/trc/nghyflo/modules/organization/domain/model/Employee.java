3ded652dd5ed7fc65ef0e71eba1e56577251ba23
Aminosoft <147345045+CHOUABBIA-AMINE@users.noreply.github.com>
2026-05-22 17:41:48 +0100

    chore(stabilization): normalize organization employee standards

--- src/main/java/dz/sh/trc/nghyflo/modules/organization/domain/model/Employee.java
+++ src/main/java/dz/sh/trc/nghyflo/modules/organization/domain/model/Employee.java
@@ -1,3 +1,21 @@
+/**
+ *
+ * @Project     : NGHyFloAPI
+ * @Product     : NGHyFlo - New Generation Hydrocarbon Flow Intelligence Platform
+ * @Author      : NGHyFlo Engineering Team
+ * @Owner       : Sonatrach / TRC Digitalization Initiative
+ *
+ * @Name        : Employee
+ * @CreatedOn   : 2026-05-21
+ * @UpdatedOn   : 2026-05-22
+ *
+ * @Type        : Class
+ * @Layer       : Domain
+ * @Package     : dz.sh.trc.nghyflo.modules.organization.domain.model
+ *
+ * @Description : Represents an operational employee assigned to an NGHyFlo organization structure.
+ *
+ */
 package dz.sh.trc.nghyflo.modules.organization.domain.model;
 
 import dz.sh.trc.nghyflo.shared.domain.value.EmployeeId;
